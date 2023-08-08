b7cfd284dde6a7cd1782a55cba221c337c49410b
Valeria R <valeria@fidel.uk>
2023-08-08 17:13:07 +0200

    Updated example projects for v 2.0.0

--- Examples/JavaExample/app/src/main/java/uk/fidel/javaexample/MainActivity.java
+++ Examples/JavaExample/app/src/main/java/uk/fidel/javaexample/MainActivity.java
@@ -2,23 +2,26 @@ package uk.fidel.javaexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 
-import com.fidel.sdk.Fidel;
-import com.fidel.sdk.LinkResult;
-import com.fidel.sdk.LinkResultError;
-import com.fidel.sdk.data.abstraction.FidelCardLinkingObserver;
+import com.fidelapi.Fidel;
+import com.fidelapi.entities.CardScheme;
+import com.fidelapi.entities.Country;
+import com.fidelapi.entities.FidelError;
+import com.fidelapi.entities.FidelResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.EnumSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String FIDEL_DEBUG_TAG = "Fidel.Debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,38 +33,41 @@ public class MainActivity extends AppCompatActivity {
 
         // On click of the button present the Fidel card linking UI
         Button linkCardButton = findViewById(R.id.btn_link_card);
-        linkCardButton.setOnClickListener(v -> Fidel.present(MainActivity.this));
+        linkCardButton.setOnClickListener(v -> Fidel.start(MainActivity.this));
     }
 
     private void setupFidelSDK() {
         Fidel.programId = "Your program ID. Please copy it from your Fidel dashboard.";
-        Fidel.apiKey = "pk_test_your_sdk_key";
+
+        /*
+         * For a real project, it is recommended to store the SDK key on your backend and retrieve it
+         * before starting to use the SDK, after the user authenticates.
+         */
+        Fidel.sdkKey = "pk_test_your_sdk_key";
 
         // Show your banner image on top of the card linking UI
         Fidel.bannerImage = BitmapFactory.decodeResource(getResources(), R.drawable.fidel_test_banner);
-        // Set the autoScan flag to true, if you'd like the SDK to open the card scanning UI
-        // immediately after presenting.
-        Fidel.autoScan = false;
+
         // The countries that you will allow users to pick from.
         // If only one country is set, users will not need to pick a country.
-        Fidel.allowedCountries = Fidel.Country.values();
+        Fidel.allowedCountries = EnumSet.of(Country.UNITED_STATES, Country.CANADA, Country.IRELAND);
         // Sets the default selected country when the user opens the card enrollment screen.
-        Fidel.defaultSelectedCountry = Fidel.Country.UNITED_STATES;
+        Fidel.defaultSelectedCountry = Country.UNITED_STATES;
         // The company name will be mentioned in the card linking consent text
         Fidel.companyName = "Cashback Inc.";
         // The privacy URL used in the card linking consent text
-        Fidel.privacyURL = "https://www.fidel.uk/";
+        Fidel.privacyPolicyUrl = "https://www.fidel.uk/";
         // If a north american country is part of the `allowedCountries` Set,
         // then this Terms & Conditions URL is mandatory
-        Fidel.termsConditionsURL = "https://fidel.uk/docs/";
+        Fidel.termsAndConditionsUrl = "https://fidel.uk/docs/";
         // The program name will be mentioned in the card linking consent text
         Fidel.programName = "Fidélité";
         // The card schemes that you are supporting (use Fidel.defaultSupportedCardSchemes to allow
         // all Fidel supported card schemes)
         Fidel.supportedCardSchemes = EnumSet.of(
-                Fidel.CardScheme.AMERICAN_EXPRESS,
-                Fidel.CardScheme.VISA,
-                Fidel.CardScheme.MASTERCARD
+                CardScheme.AMERICAN_EXPRESS,
+                CardScheme.VISA,
+                CardScheme.MASTERCARD
         );
         // The delete instructions which will be written in the card linking consent text
         Fidel.deleteInstructions = "going to your account settings";
@@ -73,44 +79,22 @@ public class MainActivity extends AppCompatActivity {
             jsonMeta.put("customKey1", "customValue1");
             jsonMeta.put("customKey2", "customValue2");
         }  catch(JSONException e) {
-            Log.e(Fidel.FIDEL_DEBUG_TAG, e.getLocalizedMessage());
+            Log.e(FIDEL_DEBUG_TAG, e.getLocalizedMessage());
         }
         Fidel.metaData = jsonMeta;
 
         // Add a card linking observer to handle a successful or failing card linking result.
-        // The alternative is to use the `onActivityResult` overridden function (see below), but that approach
-        // will send you the result only once the UI is closed.
-        Fidel.setCardLinkingObserver(new FidelCardLinkingObserver() {
-            @Override
-            public void onCardLinkingFailed(LinkResultError linkResultError) {
-                Log.e(Fidel.FIDEL_DEBUG_TAG, "Error message = " + linkResultError.message);
+        Fidel.onResult = fidelResult -> {
+            if (fidelResult instanceof FidelResult.Enrollment){
+                String cardId = ((FidelResult.Enrollment) fidelResult).getEnrollmentResult().cardId;
+                Log.d(FIDEL_DEBUG_TAG, "The card ID = " + cardId);
+            } else if(fidelResult instanceof FidelResult.Error) {
+                FidelError error = ((FidelResult.Error) fidelResult).getError();
+                Log.e(FIDEL_DEBUG_TAG, "Error message = " + error);
+            } else {
+                Log.d(FIDEL_DEBUG_TAG, "Other result: "+ fidelResult.toString());
             }
-
-            @Override
-            public void onCardLinkingSucceeded(LinkResult linkResult) {
-                Log.d(Fidel.FIDEL_DEBUG_TAG, "The link ID = " + linkResult.id);
-            }
-        });
-    }
-
-    // Override this function in your activity if you want to get the result after the card linking UI finishes
-    // The alternative (and you can get more "status" updates while the activity is still presented)
-    // is to set an observer via  Fidel.setCardLinkingObserver(FidelCardLinkingObserver). See an example above
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Use this request code to identifier the result you're trying to handle
-        if(requestCode == Fidel.FIDEL_LINK_CARD_REQUEST_CODE) {
-            if(data != null && data.hasExtra(Fidel.FIDEL_LINK_CARD_RESULT_CARD)) {
-                LinkResult linkResult = data.getParcelableExtra(Fidel.FIDEL_LINK_CARD_RESULT_CARD);
-                LinkResultError error = linkResult.getError();
-                if (error != null) {
-                    Log.e(Fidel.FIDEL_DEBUG_TAG, "Error message = " + error.message);
-                } else {
-                    Log.d(Fidel.FIDEL_DEBUG_TAG, "The link ID = " + linkResult.id);
-                }
-            }
-        }
+        };
+        Fidel.onMainActivityCreate(this);
     }
 }
--- Examples/KotlinExample/app/src/main/java/uk/fidel/kotlinexample/MainActivity.kt
+++ Examples/KotlinExample/app/src/main/java/uk/fidel/kotlinexample/MainActivity.kt
@@ -1,20 +1,25 @@
 package uk.fidel.kotlinexample
 
-import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.fidel.sdk.Fidel
-import com.fidel.sdk.LinkResult
-import com.fidel.sdk.LinkResultError
-import com.fidel.sdk.data.abstraction.FidelCardLinkingObserver
+import com.fidelapi.Fidel
+import com.fidelapi.entities.CardScheme
+import com.fidelapi.entities.Country
+import com.fidelapi.entities.FidelResult
+import com.fidelapi.entities.abstraction.OnResultObserver
 import org.json.JSONException
 import org.json.JSONObject
 import java.util.EnumSet
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val FIDEL_DEBUG_TAG: String = "Fidel.Debug"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,38 +28,40 @@ class MainActivity : AppCompatActivity() {
 
         // On click of the button present the Fidel card linking UI
         val linkCardButton = findViewById<Button>(R.id.btn_link_card)
-        linkCardButton.setOnClickListener { Fidel.present(this@MainActivity) }
+        linkCardButton.setOnClickListener { Fidel.start(this@MainActivity) }
     }
 
     private fun setupFidelSDK() {
         Fidel.programId = "Your program ID. Please copy it from your Fidel dashboard."
-        Fidel.apiKey = "pk_test_your_sdk_key"
+
+        /**
+         * For a real project, it is recommended to store the SDK key on your backend and retrieve it
+         * before starting to use the SDK, after the user authenticates.
+         */
+        Fidel.sdkKey = "pk_test_your_sdk_key"
 
         // Show your banner image on top of the card linking UI
         Fidel.bannerImage = BitmapFactory.decodeResource(resources, R.drawable.fidel_test_banner)
-        // Set the autoScan flag to true, if you'd like the SDK to open the card scanning UI
-        // immediately after presenting.
-        Fidel.autoScan = false
         // The countries that you will allow users to pick from.
         // If only one country is set, users will not need to pick a country.
-        Fidel.allowedCountries = Fidel.Country.values()
+        Fidel.allowedCountries = Country.values().toSet()
         // Sets the default selected country when the user opens the card enrollment screen.
-        Fidel.defaultSelectedCountry = Fidel.Country.UNITED_STATES
+        Fidel.defaultSelectedCountry = Country.UNITED_STATES
         // The company name will be mentioned in the card linking consent text
         Fidel.companyName = "Cashback Inc."
         // The privacy URL used in the card linking consent text
-        Fidel.privacyURL = "https://www.fidel.uk/"
+        Fidel.programName = "https://www.fidel.uk/"
         // If a north american country is part of the `allowedCountries` Set,
         // then this Terms & Conditions URL is mandatory
-        Fidel.termsConditionsURL = "https://fidel.uk/docs/"
+        Fidel.termsAndConditionsUrl = "https://fidel.uk/docs/"
         // The program name will be mentioned in the card linking consent text
         Fidel.programName = "Fidélité"
         // The card schemes that you are supporting (use Fidel.defaultSupportedCardSchemes to allow
         // all Fidel supported card schemes)
         Fidel.supportedCardSchemes = EnumSet.of(
-                Fidel.CardScheme.AMERICAN_EXPRESS,
-                Fidel.CardScheme.VISA,
-                Fidel.CardScheme.MASTERCARD
+            CardScheme.AMERICAN_EXPRESS,
+            CardScheme.VISA,
+            CardScheme.MASTERCARD
         )
         // The delete instructions which will be written in the card linking consent text
         Fidel.deleteInstructions = "going to your account settings"
@@ -68,44 +75,27 @@ class MainActivity : AppCompatActivity() {
         } catch (e: JSONException) {
             val exceptionMessage = e.localizedMessage
             if (exceptionMessage != null) {
-                Log.e(Fidel.FIDEL_DEBUG_TAG, exceptionMessage)
+                Log.e(FIDEL_DEBUG_TAG, exceptionMessage)
             }
         }
         Fidel.metaData = jsonMeta
 
         // Add a card linking observer to handle a successful or failing card linking result.
-        // The alternative is to use the `onActivityResult` overridden function (see below), but that approach
-        // will send you the result only once the UI is closed.
-        Fidel.setCardLinkingObserver(object : FidelCardLinkingObserver {
-            override fun onCardLinkingFailed(linkResultError: LinkResultError) {
-                Log.e(Fidel.FIDEL_DEBUG_TAG, "Error message = " + linkResultError.message)
-            }
+        Fidel.onResult = OnResultObserver { result ->
+            when (result) {
+                is FidelResult.Enrollment -> {
+                    Log.d(FIDEL_DEBUG_TAG, "The card ID = " + result.enrollmentResult.cardId)
+                }
 
-            override fun onCardLinkingSucceeded(linkResult: LinkResult) {
-                Log.d(Fidel.FIDEL_DEBUG_TAG, "The link ID = " + linkResult.id)
-            }
-        })
-    }
+                is FidelResult.Error -> {
+                    Log.e(FIDEL_DEBUG_TAG, "Error message = " + result.error.toString())
+                }
 
-    // Override this function in your activity if you want to get the result after the card linking UI finishes
-    // The alternative (and you can get more "status" updates while the activity is still presented)
-    // is to set an observer via  Fidel.setCardLinkingObserver(FidelCardLinkingObserver). See an example above
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Use this request code to identifier the result you're trying to handle
-        if (requestCode == Fidel.FIDEL_LINK_CARD_REQUEST_CODE) {
-            if (data != null && data.hasExtra(Fidel.FIDEL_LINK_CARD_RESULT_CARD)) {
-                val linkResult = data.getParcelableExtra(Fidel.FIDEL_LINK_CARD_RESULT_CARD) as? LinkResult
-                if (linkResult != null) {
-                    val error = linkResult.error
-                    if (error != null) {
-                        Log.e(Fidel.FIDEL_DEBUG_TAG, "Error message = " + error.message)
-                    } else {
-                        Log.d(Fidel.FIDEL_DEBUG_TAG, "The link ID = " + linkResult.id)
-                    }
+                else -> {
+                    Log.d(FIDEL_DEBUG_TAG, "Other result")
                 }
             }
         }
+        Fidel.onMainActivityCreate(this)
     }
 }
