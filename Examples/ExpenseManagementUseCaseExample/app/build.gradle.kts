62d96adc2a7e29de14c3ea90cc4d6c2050e520e4
Valeria R <valeria@fidel.uk>
2023-11-07 18:31:05 +0100

    updates sdk version

--- Examples/ExpenseManagementUseCaseExample/app/build.gradle.kts
+++ Examples/ExpenseManagementUseCaseExample/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.ProguardFiles.getDefaultProguardFile
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -36,12 +38,10 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("com.github.FidelLimited:android-sdk:2.1.0")
-
+    implementation("com.github.FidelLimited:android-sdk:2.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
